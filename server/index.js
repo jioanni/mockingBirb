a3fa2745074c4c00a73cc0cffe2f701f1c98db92
Jason Ioannides <jason.ioannides@gmail.com>
2018-04-18 15:55:29 -0400

    more stufffffs

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+const volleyball = require('volleyball')
+const bodyParser = require('body-parser')
+const api = require('./api')
+const db = require('./db')
+
+//connection status middleware
+app.use(volleyball) 
+
+//body parsing middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+//api routing middleware
+app.use('/api', require('./api'))
+
+app.use((err, req, res, next) => {
+    console.error(err)
+    console.error(err.stack)
+    res.status(err.status || 500).send(err.message || 'Internal server error.')
+  }) // Generic error handling.
+
+db.sync().then(
+    app.listen(PORT, function(){
+        console.log('listening on 3000') //logging to view server start
+    })
+)
